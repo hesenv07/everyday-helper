93a10ae574e1b3b1ed768e507c58649a0ee03119
hacagahasanli <hacagahasanli@gmail.com>
2025-12-02 15:38:55 +0400

    fix(types): package import issue

--- tsup.config.ts
+++ tsup.config.ts
@@ -7,18 +7,24 @@ export default defineConfig({
     'src/utils/_global.ts',
     'src/hooks/_global.ts',
     'src/constants/_global.ts',
-    'src/constants/_global.ts',
   ],
   format: ['esm', 'cjs'],
   dts: {
+    resolve: true,
     compilerOptions: {
-      moduleResolution: 'bundler',
+      moduleResolution: 'node10',
+      skipLibCheck: true,
+      allowSyntheticDefaultImports: true,
+      esModuleInterop: true,
     },
   },
   clean: true,
   sourcemap: true,
   minify: false, // keep readable for internal use
   outDir: 'dist',
+  splitting: false,
+  treeshake: true,
+  external: ['react', 'react-dom', 'react-router-dom', 'dayjs'],
   onSuccess: 'copyfiles -u 1 "src/styles/**/*.css" dist',
   ignoreWatch: ['**/*.test.ts', '**/*.test.tsx', '**/*.spec.ts', 'src/__tests__'],
 });
